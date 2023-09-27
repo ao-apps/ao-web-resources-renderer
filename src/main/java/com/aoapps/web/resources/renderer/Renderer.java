f5f0577375ab88769ab260aca1f2aa3d2e76a76e
AO Industries, Inc <support@aoindustries.com>
2023-09-27 08:56:17 +0000

    Implemented renderScripts

--- src/main/java/com/aoapps/web/resources/renderer/Renderer.java
+++ src/main/java/com/aoapps/web/resources/renderer/Renderer.java
@@ -1,6 +1,6 @@
 /*
  * ao-web-resources-renderer - Renders HTML for web resource management.
- * Copyright (C) 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2020, 2021, 2022, 2023  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -24,13 +24,18 @@
 package com.aoapps.web.resources.renderer;
 
 import com.aoapps.html.any.AnyLINK;
+import com.aoapps.html.any.AnySCRIPT;
+import com.aoapps.html.any.AnyScriptSupportingContent;
 import com.aoapps.html.any.AnyUnion_Metadata_Phrasing;
+import com.aoapps.html.any.Content;
 import com.aoapps.net.EmptyURIParameters;
 import com.aoapps.servlet.attribute.ScopeEE;
 import com.aoapps.servlet.lastmodified.AddLastModified;
 import com.aoapps.servlet.lastmodified.LastModifiedUtil;
 import com.aoapps.web.resources.registry.Group;
 import com.aoapps.web.resources.registry.Registry;
+import com.aoapps.web.resources.registry.Script;
+import com.aoapps.web.resources.registry.Scripts;
 import com.aoapps.web.resources.registry.Style;
 import com.aoapps.web.resources.registry.Styles;
 import java.io.IOException;
@@ -71,13 +76,15 @@ public class Renderer {
       ScopeEE.APPLICATION.attribute(Renderer.class.getName());
 
   /**
-   * Comments included when no styles written.
+   * Comments included when no styles or scripts are written.
    */
   private static final String
-      NO_REGISTRIES        = "<!-- ao-web-resources-renderer: no registries -->",
-      NO_ACTIVATIONS       = "<!-- ao-web-resources-renderer: no activations -->",
-      NO_STYLES            = "<!-- ao-web-resources-renderer: no styles -->",
-      NO_APPLICABLE_STYLES = "<!-- ao-web-resources-renderer: no applicable styles -->";
+      NO_REGISTRIES         = "<!-- ao-web-resources-renderer: no registries -->",
+      NO_ACTIVATIONS        = "<!-- ao-web-resources-renderer: no activations -->",
+      NO_STYLES             = "<!-- ao-web-resources-renderer: no styles -->",
+      NO_APPLICABLE_STYLES  = "<!-- ao-web-resources-renderer: no applicable styles -->",
+      NO_SCRIPTS            = "<!-- ao-web-resources-renderer: no scripts -->",
+      NO_APPLICABLE_SCRIPTS = "<!-- ao-web-resources-renderer: no applicable scripts -->";
 
   /**
    * Initializes the {@link Renderer} during {@linkplain ServletContextListener application start-up}.
@@ -112,25 +119,18 @@ public class Renderer {
   // TODO: Add/remove (or set/clear if only one) optimizer hooks: UnaryOperator<Set<...>>?
 
   /**
-   * Combines all the styles from {@link HttpServletRequest} and {@link HttpSession} into a single set,
-   * then renders the set of link tags.
+   * Resolve current activations.
    *
-   * @param  registeredActivations  Should the registered activations be applied?
-   *
-   * @param  activations  Additional activations applied after those configured in the registries.
-   *
-   * @param  registries  Iterated up to twice: first to determine group activations,
-   *                     then to union the styles from all activated groups.
+   * @return  The set of groups (which may be empty).
+   *          When there are no groups, a comment is written to {@code content}.
    */
-  // TODO: Support included/inherited groups
-  public void renderStyles(
-      HttpServletRequest request,
-      HttpServletResponse response,
-      AnyUnion_Metadata_Phrasing<?, ?> content,
+  private static Set<Group.Name> resolveActivations(
+      Content<?, ?> content,
       boolean registeredActivations,
       Map<Group.Name, Boolean> activations,
       Iterable<Registry> registries
   ) throws IOException {
+    Set<Group.Name> groups = new HashSet<>();
     if (logger.isLoggable(Level.FINER)) {
       logger.finer("registries = " + registries);
     }
@@ -140,8 +140,6 @@ public class Renderer {
       }
       content.unsafe(NO_REGISTRIES); // TODO: comment method
     } else {
-      // Resolve current activations
-      Set<Group.Name> groups = new HashSet<>();
       if (registeredActivations) {
         boolean hasRegistry = false;
         for (Registry registry : registries) {
@@ -164,7 +162,8 @@ public class Renderer {
             logger.finer(NO_REGISTRIES);
           }
           content.unsafe(NO_REGISTRIES); // TODO: comment method
-          return;
+          assert groups.isEmpty();
+          return groups;
         }
       }
       if (activations != null) {
@@ -188,8 +187,34 @@ public class Renderer {
           logger.finer(NO_ACTIVATIONS);
         }
         content.unsafe(NO_ACTIVATIONS); // TODO: comment method
-        return;
       }
+    }
+    return groups;
+  }
+
+  /**
+   * Combines all the styles from {@link HttpServletRequest} and {@link HttpSession} into a single set,
+   * then renders the set of link tags.
+   *
+   * @param  registeredActivations  Should the registered activations be applied?
+   *
+   * @param  activations  Additional activations applied after those configured in the registries.
+   *
+   * @param  registries  Iterated up to twice: first to determine group activations,
+   *                     then to union the styles from all activated groups.
+   */
+  // TODO: Support included/inherited groups
+  public void renderStyles(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AnyUnion_Metadata_Phrasing<?, ?> content,
+      boolean registeredActivations,
+      Map<Group.Name, Boolean> activations,
+      Iterable<Registry> registries
+  ) throws IOException {
+    // Resolve current activations
+    Set<Group.Name> groups = resolveActivations(content, registeredActivations, activations, registries);
+    if (!groups.isEmpty()) {
       // Find all the styles for all the activated groups in all registries
       List<Styles> allStyles = new ArrayList<>();
       boolean hasRegistry = false;
@@ -276,6 +301,7 @@ public class Renderer {
                     )
                 )
                 .media(style.getMedia())
+                .crossorigin(style.getCrossorigin())
                 .disabled(style.isDisabled())
                 .__();
           }
@@ -328,6 +354,158 @@ public class Renderer {
     );
   }
 
-  // TODO: renderScriptsHeadEnd
-  // TODO: renderScriptsBodyEnd
+  /**
+   * Combines all the scripts from {@link HttpServletRequest} and {@link HttpSession} into a single set,
+   * then renders the set of script tags.
+   *
+   * @param  registeredActivations  Should the registered activations be applied?
+   *
+   * @param  activations  Additional activations applied after those configured in the registries.
+   *
+   * @param  registries  Iterated up to twice: first to determine group activations,
+   *                     then to union the scripts from all activated groups.
+   */
+  // TODO: Support included/inherited groups
+  public void renderScripts(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AnyScriptSupportingContent<?, ?> content,
+      boolean registeredActivations,
+      Map<Group.Name, Boolean> activations,
+      Script.Position position,
+      Iterable<Registry> registries
+  ) throws IOException {
+    // Resolve current activations
+    Set<Group.Name> groups = resolveActivations(content, registeredActivations, activations, registries);
+    if (!groups.isEmpty()) {
+      // Find all the scripts for all the activated groups in all registries
+      List<Scripts> allScripts = new ArrayList<>();
+      boolean hasRegistry = false;
+      for (Registry registry : registries) {
+        if (registry != null) {
+          hasRegistry = true;
+          for (Group.Name name : groups) {
+            Group group = registry.getGroup(name, false);
+            if (logger.isLoggable(Level.FINEST)) {
+              logger.finest("name: " + name + ", group: " + group);
+            }
+            if (group != null) {
+              allScripts.add(group.scripts);
+            }
+          }
+        }
+      }
+      if (!hasRegistry) {
+        if (logger.isLoggable(Level.FINER)) {
+          logger.finer(NO_REGISTRIES);
+        }
+        content.unsafe(NO_REGISTRIES); // TODO: comment method
+        return;
+      }
+      // Perform a union of all scripts while filtering by position
+      int size = allScripts.size();
+      if (size == 0) {
+        if (logger.isLoggable(Level.FINER)) {
+          logger.finer(NO_SCRIPTS);
+        }
+        content.unsafe(NO_SCRIPTS); // TODO: comment method
+      } else {
+        // TODO: How early can we filter for position (and same thing for direction of styles)?
+        Scripts scripts;
+        if (size == 1) {
+          scripts = allScripts.get(0);
+          if (logger.isLoggable(Level.FINEST)) {
+            logger.finest("direct scripts: " + scripts);
+          }
+        } else {
+          scripts = Scripts.union(allScripts);
+          if (logger.isLoggable(Level.FINEST)) {
+            logger.finest("unioned scripts: " + scripts);
+          }
+        }
+        Set<Script> sorted = scripts.getSorted();
+        if (logger.isLoggable(Level.FINER)) {
+          logger.finer("sorted: " + scripts);
+        }
+        // TODO: Call optimizer hook
+        boolean hasScript = false;
+        boolean didOne = false;
+        for (Script script : sorted) {
+          hasScript = true;
+          // Filter for position
+          if (script.getPosition() == position) {
+            didOne = true;
+            // TODO: Support inline scripts
+            String src = script.getUri();
+            content.script(AnySCRIPT.Type.APPLICATION_JAVASCRIPT)
+                .src(src == null ? null :
+                    LastModifiedUtil.buildURL(
+                        servletContext,
+                        request,
+                        response,
+                        "/", // TODO: contextPath here to handle ../ breaking out of application?
+                        // TODO: All buildUrl add contextPath to servlet path to support ../ outside of application generally?
+                        // TODO: / is prefixed with contextPath, so due to lack of normalization: /../ would effectively be relative to the current contextPath
+                        src,
+                        EmptyURIParameters.getInstance(),
+                        AddLastModified.AUTO,
+                        false,
+                        false
+                    )
+                )
+                .async(script.isAsync())
+                .defer(script.isDefer())
+                .crossorigin(script.getCrossorigin())
+                .__();
+          }
+        }
+        if (!didOne) {
+          if (!hasScript) {
+            if (logger.isLoggable(Level.FINER)) {
+              logger.finer(NO_SCRIPTS);
+            }
+            content.unsafe(NO_SCRIPTS); // TODO: comment method
+          } else {
+            if (logger.isLoggable(Level.FINER)) {
+              logger.finer(NO_APPLICABLE_SCRIPTS);
+            }
+            content.unsafe(NO_APPLICABLE_SCRIPTS); // TODO: comment method
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Combines all the scripts from {@link HttpServletRequest} and {@link HttpSession} into a single set,
+   * then renders the set of script tags.
+   *
+   * @param  registeredActivations  Should the registered activations be applied?
+   *
+   * @param  activations  Additional activations applied after those configured in the registries.
+   *
+   * @param  registries  Iterated up to twice: first to determine group activations,
+   *                     then to union the scripts from all activated groups.
+   *
+   * @see  #renderScripts(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, com.aoapps.html.any.AnyScriptSupportingContent, boolean, java.util.Map, com.aoapps.web.resources.registry.Script.Position, java.lang.Iterable)
+   */
+  public void renderScripts(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AnyScriptSupportingContent<?, ?> content,
+      boolean registeredActivations,
+      Map<Group.Name, Boolean> activations,
+      Script.Position position,
+      Registry ... registries
+  ) throws IOException {
+    renderScripts(
+        request,
+        response,
+        content,
+        registeredActivations,
+        activations,
+        position,
+        (registries == null) ? null : Arrays.asList(registries)
+    );
+  }
 }
